7ee1d8bf11ac64205bf231ea3e065c2a7dd87ba5
Edmund <40037900+FreedomGuard224@users.noreply.github.com>
2019-09-16 12:15:31 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Cars
+{
+    public class Class1
+    {
+        private string _make;
+        private string _model;
+        private int _year;
+
+        public Class1()
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+        }
+        public Class1(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+        public string Make
+        {
+            get
+            {
+                return _make;
+            }
+
+            set
+            {
+                _make = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+
+            set
+            {
+                _model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+
+            set
+            {
+                _year = value;
+            }
+        }
+    }
+}
